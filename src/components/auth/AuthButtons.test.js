a159e1d50cf9d060439ab0c5bb2aa3653a45510f
Parker <parkermdinsmore@gmail.com>
2024-03-14 10:38:42 -0700

    add tests around signin

---
+++ src/components/auth/AuthButtons.test.js
@@ -0,0 +1,65 @@
+import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { createServer } from "../../test/server";
+
+import AuthButtons from "./AuthButtons";
+
+const renderComponent = async () => {
+  render(
+    <MemoryRouter>
+      <AuthButtons />
+    </MemoryRouter>
+  )
+
+  await screen.findAllByRole("link");
+}
+
+describe("when user is not signed in", () => {
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: null };
+      },
+    },
+  ]);
+
+  test("the sign in and sign up buttons are visible", async () => {
+    await renderComponent()
+
+    const signInButton = screen.getByRole("link", { name: /sign in/i });
+    const signUpButton = screen.getByRole("link", { name: /sign up/i });
+
+    expect(signInButton).toBeInTheDocument()
+    expect(signInButton).toHaveAttribute("href", "/signin")
+    expect(signUpButton).toBeInTheDocument()
+    expect(signUpButton).toHaveAttribute("href", "/signup")
+  });
+
+  test("the sign out button is not visible", async () => {
+    await renderComponent()
+
+    // query by role returns null if not found (getByRole would potentially throw an error)
+    const signOutButton = screen.queryByRole("link", { name: /sign out/i });
+    expect(signOutButton).not.toBeInTheDocument()
+  });
+});
+
+describe("when user is signed in", () => {
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 7, email: "guy@flavortown.com" } };
+      },
+    },
+  ]);
+
+  test("the sign in and sign up buttons are not visible", async () => {
+    await renderComponent()
+  });
+
+  test("the sign out button is visible", async () => {
+    await renderComponent()
+  });
+});
